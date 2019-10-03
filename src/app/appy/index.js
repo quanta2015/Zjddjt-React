3fdd5b976f0e38dda53eeec5346d3500f54fef87
liyangtom <quanta2006@163.com>
2019-10-03 19:01:48 +0800

    修改服务器微信接口

--- src/app/appy/index.js
+++ src/app/appy/index.js
@@ -6,17 +6,35 @@ import "./index.less";
 import moment from "moment";
 import { toJS } from "mobx";
 
+var code 
 
 class Appy extends React.Component {
   constructor(props) {
     super(props);
+    
+
+    this.state = {
+      code: code
+    }
+  }
+
+  async UNSAFE_componentWillMount() {
+    const params = new URLSearchParams(this.props.location.search)
+    code = params.get("code")
+    this.setState({
+      code: code
+    })
   }
 
   render() {
+
+    let {code} = this.state
+    console.log(code)
+
     return (
       <div className='g-appy'>
-        appy
-        
+        apply
+        <div className="">{code}</div>
 
         
       </div>
