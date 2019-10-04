aa24ffafd641467ba0e07edd2699414b3a905f00
HytonightYX <whosiyuan@qq.com>
2019-10-04 19:05:21 +0800

    hsy feat: intr 模块 (少背景图)

--- src/app/intr/index.js
+++ src/app/intr/index.js
@@ -1,15 +1,6 @@
 import React from "react";
-import { observer, inject } from "mobx-react";
-import { Input, Tabs, Form, Button, DatePicker, Select, InputNumber, Modal, message, Skeleton, Typography } from "antd";
-
 import "./index.less";
-import moment from "moment";
-import { toJS } from "mobx";
-// import INFO from '/constant/info';
-// import A01 from './assets/A01.png'
-// import A02 from './assets/A02.jpg'
-// import DD_LOGO from './assets/dd-logo.svg'
-
+import assets from './assets'
 
 class Intr extends React.Component {
   constructor(props) {
@@ -19,10 +10,50 @@ class Intr extends React.Component {
   render() {
     return (
       <div className='g-intr'>
+        <div className='bg-wrap'>
+          <div className='logo-content'>
+            <span><img className='dd-logo' src={assets.DD_LOGO} alt=""/></span>
+            <div className='divide'/>
+            <h3>{assets.INFO.COM_NAME}</h3>
+          </div>
+        </div>
 
-        
+        <div className='content-wrap'>
+          <h3>单位简介</h3>
+          {assets.INFO.ABOUT_US}
+        </div>
 
-        
+        <div className='reason-warp'>
+          <div className='reason-title'>
+            <div className="divide-row"/>
+            <h3>选择我们的理由</h3>
+            <div className="divide-row"/>
+          </div>
+
+          <div className='reason-desc'>
+            <p>自主研发产品拥有多项国家级专利</p>
+            <p>产品通过ISO9001质量管理体系认证</p>
+          </div>
+
+          <div className='content-wrap'>
+
+            <h3 className='center'>服务</h3>
+            <div className='reason-text center'>定制加装电梯一站式服务 环节清晰 价格亲民</div>
+
+            <h3 className='center'>设计</h3>
+            <div className='reason-text center'>建筑工程设计甲级与总承包资质</div>
+
+            <h3 className='center'>采购</h3>
+            <div className='reason-text center'>任意选择心仪的电梯品牌</div>
+
+            <h3 className='center'>施工</h3>
+            <div className='reason-text center'>有丰富经验施工团队，安全可靠</div>
+
+          </div>
+
+        </div>
+
+        <div className='holder'/>
       </div>
     )
   }
