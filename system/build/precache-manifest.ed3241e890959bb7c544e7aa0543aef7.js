95cec43c8f192dbe6af150c3e67a5447a81cf8f0
liyangtom <quanta2006@163.com>
2019-10-25 20:47:25 +0800

    删除联系人

---
+++ system/build/precache-manifest.ed3241e890959bb7c544e7aa0543aef7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08eb027b3d36d9cb00c2651a6ca46070",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9c5b285754d16b1ddff",
+    "url": "/static/css/2.4980b409.chunk.css"
+  },
+  {
+    "revision": "111391a85b38ad345317",
+    "url": "/static/css/main.bdad0dc3.chunk.css"
+  },
+  {
+    "revision": "c9c5b285754d16b1ddff",
+    "url": "/static/js/2.12702383.chunk.js"
+  },
+  {
+    "revision": "111391a85b38ad345317",
+    "url": "/static/js/main.8d1715b4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6caca68f8c9b187ec5acabc176e3cb10",
+    "url": "/static/media/keyboard.6caca68f.svg"
+  },
+  {
+    "revision": "0aca3eda38304a99ca84b734d0af6471",
+    "url": "/static/media/more.0aca3eda.svg"
+  }
+]);
--- system/src/constant/apis.js
+++ system/src/constant/apis.js
@@ -1,5 +1,5 @@
 var mode = process.env.REACT_APP_MY_VAR
-var API_SERVER = ''
+var API_SERVER = 'http://47.111.22.103:8080'
 
 if (mode === 'development') {
   API_SERVER = 'http://127.0.0.1:8080'
